270e7a42bc170c0e76c0da91bd31abb7089c4c80
soniakaushal <sonia.kaushal@katerra.com>
2019-03-13 09:49:43 +0530

    Pyramid

---
+++ miscallaneous/pyramid.js
@@ -0,0 +1,33 @@
+/*
+Print Pyramid(1)
+    '#'
+Pyramid(2)
+'#'
+'###'
+Pyramid(3)
+'#'
+'###'
+'#####'
+    0   1   2
+0       *   
+1   *   *   *
+2   *   *   *
+*/
+function pyramid(n){
+    const midpoint = Math.floor((2*n-1)/2);
+    for(i=0;i<n;i++){
+        let level = '';
+        for(j=0;j<2*n-1;j++){
+            if(midpoint-i<=j  && midpoint+i>=j){
+                    level+='#';
+            }
+            else{
+                level+=' ';
+            }
+        }
+        console.log(level);
+    }
+
+}
+pyramid(5);
+
