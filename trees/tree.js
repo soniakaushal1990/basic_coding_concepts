de002c8e0100c91182730e7afe66c0c713856e08
soniakaushal <sonia.kaushal@katerra.com>
2019-12-28 12:20:51 +0530

    Graphs

--- trees/tree.js
+++ trees/tree.js
@@ -30,9 +30,11 @@ class Tree{
 }
  const node = new Node("10");
  node.add("20");
+ node.children[0].add(70);
  node.add("30");
  node.add("40");
  node.add("50");
+ console.log(node.children[0]);
  const tree = new Tree();
  tree.root = node;
 tree.traverseBF();
