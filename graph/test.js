d85284345f376cdecc855298e97d91992b388175
sonia <soniakaushal1990@gmail.com>
2020-07-16 23:40:33 +0530

    Test

--- graph/test.js
+++ graph/test.js
@@ -1,3 +1,10 @@
+// This problem was asked by Amazon.
+
+// Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
+
+// For example, this matrix has 4 islands.
+
+
 function getNumberOfIslands(mtx) {
   const rows = mtx.length
   const cols = mtx[0].length
@@ -11,7 +18,7 @@ function getNumberOfIslands(mtx) {
       }
     }
   }
-  return islands
+  return islands;
 }
 
 function markVisited(i, j, mtx, rows, cols) {
