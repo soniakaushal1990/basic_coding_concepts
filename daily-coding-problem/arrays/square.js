c73d9a63bad821260b92d59b83e19d5c50a354a3
sonia <soniakaushal1990@gmail.com>
2020-08-18 16:50:39 +0530

    square.js

---
+++ daily-coding-problem/arrays/square.js
@@ -0,0 +1,16 @@
+// Problem
+// This problem was asked by Google.
+
+// Given a sorted list of integers, square the elements and give the output in sorted order.
+
+// For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+
+function square(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i] * array[i];
+  }
+  array.sort((a, b) => a - b);
+  return array;
+}
+
+console.log(square([-9, -2, 0, 2, 3]));
