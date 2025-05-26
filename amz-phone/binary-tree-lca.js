a5922b714125b25c5d2361ab098ad90617227f12
Sonia Kaushal <soniakaushal1990@gmail.com>
2025-05-26 18:26:11 +0100

    Reorganize Repo

--- amz-phone/binary-tree-lca.js
+++
@@ -1,8 +0,0 @@
-function lca(root){
-    if(!root || root == p || root == q) return root;
-    let resL = lowestCommonAncestor(root.left,p,q);
-    let resR = lowestCommonAncestor(root.right,p,q);
- 
-    
-    return (resL && resR) ? root : (resL || resR);
-}
